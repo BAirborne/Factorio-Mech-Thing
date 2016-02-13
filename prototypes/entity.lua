a35718c9b22cf1ac7d76203bb90157e87528caac
BAirborne <farrowcguy@gmail.com>
2016-02-13 01:19:21 -0600

    Changed Icon and added support for frames for animation

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -28,7 +28,7 @@ data:extend({
           {
             width = 200,
             height = 200,
-            frame_count = 1,
+            frame_count = 4,
             direction_count = 64,
             shift = {0, -0.1875},
             animation_speed = 8,
@@ -37,43 +37,82 @@ data:extend({
             {
               {
                filename = "__mech__/graphics/mech_torso1.png",
-               width_in_frames = 1,
+               width_in_frames = 4,
                height_in_frames = 10,
               },
               {
                filename = "__mech__/graphics/mech_torso2.png",
-               width_in_frames = 1,
+               width_in_frames = 4,
                height_in_frames = 10,
               },
               {
                filename = "__mech__/graphics/mech_torso3.png",
-               width_in_frames = 1,
+               width_in_frames = 4,
                height_in_frames = 10,
               },
               {
                filename = "__mech__/graphics/mech_torso4.png",
-               width_in_frames = 1,
+               width_in_frames = 4,
                height_in_frames = 10,
               },
               {
                filename = "__mech__/graphics/mech_torso5.png",
-               width_in_frames = 1,
+               width_in_frames = 4,
                height_in_frames = 10,
               },
               {
                filename = "__mech__/graphics/mech_torso6.png",
-               width_in_frames = 1,
+               width_in_frames = 4,
                height_in_frames = 10,
               },
               {
                filename = "__mech__/graphics/mech_torso7.png",
-               width_in_frames = 1,
+               width_in_frames = 4,
                height_in_frames = 4,
               }
             }
           }
         }
       },
+
+      turret_animation =
+      {
+        layers =
+        {
+          {
+            filename = "__base__/graphics/entity/tank/turret.png",
+            line_length = 8,
+            width = 92,
+            height = 69,
+            frame_count = 1,
+            direction_count = 64,
+            shift = {-0.15625, -1.07812},
+            animation_speed = 8,
+          },
+          {
+            filename = "__base__/graphics/entity/tank/turret-mask.png",
+            line_length = 8,
+            width = 38,
+            height = 29,
+            frame_count = 1,
+            apply_runtime_tint = true,
+            direction_count = 64,
+            shift = {-0.15625, -1.23438},
+          },
+          {
+            filename = "__base__/graphics/entity/tank/turret-shadow.png",
+            line_length = 8,
+            width = 95,
+            height = 67,
+            frame_count = 1,
+            draw_as_shadow = true,
+            direction_count = 64,
+            shift = {1.70312, 0.640625},
+          }
+        }
+      },
+      turret_rotation_speed = 0.45 / 60,
+      turret_return_timeout = 300,
       rotation_speed = 0.007,
       tank_driving = true,
       weight = 700,

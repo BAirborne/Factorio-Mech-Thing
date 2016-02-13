90447efa9935e7315d94d35946eb240eb3426376
BAirborne <farrowcguy@gmail.com>
2016-02-12 20:06:14 -0600

    Changed handling of mech

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -10,8 +10,8 @@ data:extend({
       collision_box = {{-0.9, -1.3}, {0.9, 1.3}},
       selection_box = {{-0.9, -1.3}, {0.9, 1.3}},
       energy_per_hit_point = 1,
-      acceleration_per_energy = 0.15,
-      braking_power = "200kW",
+      acceleration_per_energy = 0.30,
+      braking_power = "400kW",
       effectivity = 0.5,
       burner =
         {
@@ -26,25 +26,56 @@ data:extend({
         layers =
         {
           {
-            width = 409,
-            height = 409,
+            width = 200,
+            height = 200,
             frame_count = 1,
-            direction_count = 16,
+            direction_count = 64,
             shift = {0, -0.1875},
             animation_speed = 8,
             max_advance = 0.2,
             stripes =
             {
               {
-               filename = "__mech__/graphics/mech_torso.png",
-               width_in_frames = 5,
+               filename = "__mech__/graphics/mech_torso7.png",
+               width_in_frames = 1,
                height_in_frames = 4,
+              },
+              {
+               filename = "__mech__/graphics/mech_torso6.png",
+               width_in_frames = 1,
+               height_in_frames = 10,
+              },
+              {
+               filename = "__mech__/graphics/mech_torso5.png",
+               width_in_frames = 1,
+               height_in_frames = 10,
+              },
+              {
+               filename = "__mech__/graphics/mech_torso4.png",
+               width_in_frames = 1,
+               height_in_frames = 10,
+              },
+              {
+               filename = "__mech__/graphics/mech_torso3.png",
+               width_in_frames = 1,
+               height_in_frames = 10,
+              },
+              {
+               filename = "__mech__/graphics/mech_torso2.png",
+               width_in_frames = 1,
+               height_in_frames = 10,
+              },
+              {
+               filename = "__mech__/graphics/mech_torso1.png",
+               width_in_frames = 1,
+               height_in_frames = 10,
               }
             }
           }
         }
       },
-      rotation_speed = 0.005,
+      rotation_speed = 0.007,
+      tank_driving = true,
       weight = 700,
       inventory_size = 80
   }
